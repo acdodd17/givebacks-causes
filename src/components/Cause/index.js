f665ec3157cea16fbf7bfb5f20cd507698b4121c
acdodd17 <acdodd17@gmail.com>
2022-07-01 12:40:55 -0400

    linking details and join btns

--- src/components/Cause/index.js
+++ src/components/Cause/index.js
@@ -12,8 +12,8 @@ const Cause = ({cause}) => {
                 <p className='cause-address'>{cause.address}</p>
             </div>
             <div className='cause-links'>
-                <button className='details-btn'>Details</button>
-                <button className='details-btn'>Join</button>
+                <button className='btn'><a href={cause.details_url} rel="noreferrer" target='_blank' >Details</a></button>
+                <button className='btn'><a href={cause.join_url} rel="noreferrer" target='_blank' >Join</a></button>
             </div>
         </div>
     );
